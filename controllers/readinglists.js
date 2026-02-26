fb173dc80a9c774b0f6dca7382834648ed3ed52b
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2026-02-26 22:47:21 +0900

    13.23

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,5 +1,6 @@
 const readinglistsRouter = require("express").Router();
-const { ReadingList } = require("../models");
+const { ReadingList, User } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 readinglistsRouter.post("/", async (req, res) => {
   const readingList = await ReadingList.create(req.body);
@@ -7,4 +8,14 @@ readinglistsRouter.post("/", async (req, res) => {
   res.status(201).json(readingList);
 });
 
+readinglistsRouter.put("/:id", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const readingList = await ReadingList.findByPk(req.params.id);
+  if (readingList.userId !== user.id) {
+    return res.status(401).json({ error: "not allowed" });
+  }
+  readingList.read = req.body.read;
+  await readingList.save();
+  res.status(201).json(readingList);
+});
 module.exports = readinglistsRouter;
